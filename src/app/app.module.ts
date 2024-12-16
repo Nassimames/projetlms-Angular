65c9725cf3028c3c46c651467480e0402e6045c0
Nassima <nassimamesbah0@gmail.com>
2024-12-16 14:36:14 +0100

    last upd

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,35 +1,33 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import { RouterModule } from '@angular/router';
 import { MatIconModule } from '@angular/material/icon'; // Import MatIconModule
+import { MatDatepickerModule } from '@angular/material/datepicker';
+import { MatNativeDateModule } from '@angular/material/core';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { AppRoutingModule } from './app-routing.module'; // Import routing module
+
+// Components
 import { LoginComponent } from './login/login.component';
 import { SignupComponent } from './signup/signup.component';
 import { ForgotPasswordComponent } from './forgot-password/forgot-password.component';
 import { ResetPasswordCodeComponent } from './reset-password-code/reset-password-code.component';
-import { AppRoutingModule } from './app-routing.module'; // Import routing module
-import { MatDatepickerModule } from '@angular/material/datepicker';
-import { MatNativeDateModule } from '@angular/material/core';
-import { FormsModule } from '@angular/forms';  // Importer FormsModule
-import { ReactiveFormsModule } from '@angular/forms';
-import { AuthService } from '../app/services/auth.service';  // Assurez-vous que le chemin est correct
-
-// Standalone components
 import { LayoutComponent } from './layout/layout.component'; 
-import { CourseListComponent } from './course-list/course-list.component'; // Importez CourseListComponent ici
-import { CourseDetailsComponent } from './course-details/course-details.component';  // Importez CourseDetailsComponent ici
-import { HomeComponent } from './home/home.component';  // Importez HomeComponent ici
+import { CourseListComponent } from './course-list/course-list.component'; 
+import { CourseDetailsComponent } from './course-details/course-details.component';  
+import { HomeComponent } from './home/home.component'; 
 import { CalendarComponent } from './calendar/calendar.component';
-import { BadgesComponent } from './badges/badges.component'; // Importez BadgesComponent ici
-// Routes
-import { routes } from './app.routes';
+import { BadgesComponent } from './badges/badges.component';
+
+// Services
+import { AuthService } from './services/auth.service';  // Assurez-vous que le chemin est correct
 
 @NgModule({
   declarations: [
-    SignupComponent,
     LoginComponent,
+    SignupComponent,
     ForgotPasswordComponent,
     ResetPasswordCodeComponent,
-    // N'incluez pas CourseListComponent, HomeComponent, BadgesComponent ici
+    // Déclarez ici les composants de page
   ],
   imports: [
     ReactiveFormsModule,  // Ajoute ReactiveFormsModule si nécessaire
@@ -37,14 +35,8 @@ import { routes } from './app.routes';
     MatDatepickerModule,
     MatNativeDateModule,
     BrowserModule,
-    RouterModule.forRoot(routes), // Configure routes
-    MatIconModule, // For Material icons
-    AppRoutingModule, // Routing module
-    LayoutComponent, // Add standalone components to imports
-    CourseListComponent,  // Import CourseListComponent ici dans les imports
-    HomeComponent,  // Import HomeComponent ici dans les imports
-    CourseDetailsComponent, // Import CourseDetailsComponent ici dans les imports
-    BadgesComponent,  // Import BadgesComponent ici dans les imports
+    AppRoutingModule,  // Import du module de routage
+    MatIconModule,  // Pour les icônes Material
   ],
   providers: [
     AuthService,  // Fournisseur de services
